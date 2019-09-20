f3fda8ca4a6d24479d6dc2fb132c4320b0f25bf6
hkocharyan <diavarus@gmail.com>
2019-09-20 13:51:12 +0400

    updated test.sh

--- test.sh
+++ test.sh
@@ -1,6 +1,2 @@
 #replace awk or fmt part with 'lowercase' or 'makewords' program of your own to test
-<<<<<<< HEAD
-cat text_to_spell | ./lowercase | fmt -1 | uniq | sort| sed 's/\.//g' | comm -13 dictionary -
-=======
-cat text_to_spell | awk '{print tolower($0)}' | fmt -1 | uniq | sort| sed 's/\.//g' | comm -13 dictionary -
->>>>>>> 431f78b594e841405811c6279314e0c3f7502ade
+cat text_to_spell | ./lowercase | ./makewords | uniq | sort| sed 's/\.//g' | comm -13 dictionary -
